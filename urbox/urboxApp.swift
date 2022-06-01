0e93ba5aa7c0bd4190614d854d275a984d55e957
Chi Dung <chidung091@gmail.com>
2022-06-01 20:20:58 +0700

    Initial Commit

---
+++ urbox/urboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  urboxApp.swift
+//  urbox
+//
+//  Created by Chi Dung on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct urboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
